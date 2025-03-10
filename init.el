c4cb5eac50e7cac595ece4859920c816c9a05d59
Alain Chevanier <alain.chevanier@ciencias.unam.mx>
2025-03-09 22:15:21 -0600

    stuff

--- init.el
+++ init.el
@@ -157,7 +157,7 @@
        ;;ocaml             ; an objective camel
        (org +pretty)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
+       plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
@@ -175,7 +175,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
        (clojure +lsp)      ; clojure lisp
 
